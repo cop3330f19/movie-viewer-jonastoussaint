4c03a264a969deacee917c1921f80c5cf7861fd9
Jonas Toussaint <jonas.toussaint91@gmail.com>
2019-12-03 23:19:47 -0500

    Second Commit

---
+++ src/main/java/com/mycompany/movies/models/Movies.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.movies.models;
+import info.movito.themoviedbapi.TmdbApi;
+import info.movito.themoviedbapi.TmdbMovies;
+import info.movito.themoviedbapi.model.MovieDb;
+import info.movito.themoviedbapi.model.Video;
+import info.movito.themoviedbapi.model.core.MovieResultsPage;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Jonas
+ */
+public class Movies {
+    //TMBD API Connection
+    private  static final TmdbApi CONNECTION = new TmdbApi("bc6da920763d6d23601c2892db37f97a");
+    
+    private static final String LANGUAGE = "en-US";
+    
+    //Return the list of currently playing movies
+    
+    public ArrayList<MovieDb> getMovies()
+    {  
+        TmdbMovies movies = new TmdbMovies(CONNECTION);
+        MovieResultsPage result = movies.getNowPlayingMovies(LANGUAGE, 1, "");
+    
+        return (ArrayList<MovieDb>) result.getResults();
+    }
+      
+    //get Trailers
+    public ArrayList<Video> getVideos(int movieId)
+    {
+        TmdbMovies movies = new TmdbMovies(CONNECTION);
+        return (ArrayList<Video>) movies.getVideos(movieId, LANGUAGE);
+    }
+ 
+}
